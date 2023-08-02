128270fbe57278dee273a5018b3017a7c01c4fa8
Roald Ong <roald.ong@gmail.com>
2023-08-02 13:11:20 +0800

    first initial working version with rclnodejs

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 const { app, BrowserWindow } = require('electron')
-
-
+const { resolve } = require('path');
+const rclnodejs = require('rclnodejs');
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        // webPreferences: {
+        //     preload: path.join(__dirname, 'ros.js')
+        // }
     })
 
     win.loadFile('index.html')
 }
 
+
 app.whenReady().then(() => {
     createWindow()
+    rclnodejs.init().then(() => {
+
+        const node = new rclnodejs.Node('publisher_example_node');
+        const publisher = node.createPublisher('std_msgs/msg/String', 'topic');
+
+        let counter = 0;
+        setInterval(() => {
+            console.log(`Publishing message: Hello ROS ${counter}`);
+            publisher.publish(`Hello ROS ${counter++}`);
+        }, 1000);
+
+        node.spin();
+
+    });
 })
